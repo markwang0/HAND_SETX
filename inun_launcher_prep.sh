20e75e5ff7604f6977f1b54271856b3c300ca64b
markwang0 <71631906+markwang0@users.noreply.github.com>
2023-01-19 13:07:47 -0600

    update inundation launcher scripts

--- inun_launcher_prep.sh
+++ inun_launcher_prep.sh
@@ -1,19 +1,29 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# make subdirectories for inudnation .tif files
+# get name identifier of run with -n flag
+# e.g. ./inun_launcher_prep.sh -n harvey
+# if none given use date and time
+while getopts n: flag; do
+    case "${flag}" in
+        n) name=${OPTARG};;
+    esac
+done
+
+if (( $OPTIND == 1 )); then
+    name=$(date +%Y%m%d-%H%M%S)
+fi
+
+echo "Identifier: $name"
+
 # create file of launcher commands
-
+# make subdirectories for inundation .tif files
 if [ -f inun_launcher.sh ]; then
     rm inun_launcher.sh
 fi
 
-# calculation identifier, change each run
-# using flows sent from Tim on 2022-12-21
-export calc_id=20221221
-
 for D in 12*; do
     if [ -d "${D}" ]; then
-        mkdir -p ${D}/inundation_${calc_id}/mod{1,2a,2b,3a,3b}
-        echo "python calc_inun.py \"${D}\" \"${calc_id}\"" >> inun_launcher.sh
+        mkdir -p ${D}/inundation/${name}
+        echo "python calc_inun.py \"${D}\" \"${name}\"" >> inun_launcher.sh
     fi
 done
