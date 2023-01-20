bcfedbaa23f32c652c458b7dc8ce3c75e79d445a
markwang0 <71631906+markwang0@users.noreply.github.com>
2023-01-20 08:47:16 -0600

    ignore csv

--- all_src_hydrotable_lmtQ.py
+++ all_src_hydrotable_lmtQ.py
@@ -3,6 +3,6 @@ from glob import glob
 
 ht_paths = glob("./12*/hydroTable_rp_bf_lmtdischarge_cda.csv")
 pd.concat([pd.read_csv(p) for p in ht_paths]).to_csv(
-    "AustinExerciseModules_rev/hydroTable_all_rp_bf_lmtdischarge_cda.csv",
+    "./hydroTable_all_rp_bf_lmtdischarge_cda.csv",
     index=False,
 )
