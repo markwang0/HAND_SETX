23b809b09c2db38c40a8ec3ecd86edf7a59f6666
markwang0 <71631906+markwang0@users.noreply.github.com>
2023-01-25 11:09:21 -0600

    crop 10m DEM

--- crop_all_hand.sh
+++ crop_all_hand.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
 
-# run crop_hand.py in each HUC8 directory
-# HUC8 directories of interest begin with 12*
-# activate hand-austin-exercises conda environment first
+# run crop_hand.py in each HUC directory
+# HUC directories of interest begin with 12*
+# activate conda environment first
 
-for D in 12*; do
+# 3m Fathom DEM
+# for D in 3m/12*; do
+#     if [ -d "${D}" ]; then
+#         cd "${D}"
+#         cp /work2/08291/mwa/stampede2/HAND_SETX/crop_hand.py .
+#         python crop_hand.py
+#         echo "${D} done"
+#         cd /work2/08291/mwa/stampede2/HAND_SETX/
+#     fi
+# done
+
+# 10m USGS DEM
+for D in 10m/12*; do
     if [ -d "${D}" ]; then
         cd "${D}"
         cp /work2/08291/mwa/stampede2/HAND_SETX/crop_hand.py .
         python crop_hand.py
         echo "${D} done"
-        cd ..
+        cd /work2/08291/mwa/stampede2/HAND_SETX/
     fi
 done
