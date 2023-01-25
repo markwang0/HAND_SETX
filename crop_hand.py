beb94fd5fb5e7ff8dec7a2c13e002348a3782e78
markwang0 <71631906+markwang0@users.noreply.github.com>
2023-01-25 11:09:21 -0600

    crop 10m DEM

--- crop_hand.py
+++ crop_hand.py
@@ -1,4 +1,4 @@
-# crop HUC8 HAND rasters into component FATSGTID watersheds
+# crop HAND HUC rasters into component FATSGTID (3m) / COMID (10m) watersheds
 # healed and unhealed HAND
 
 import geopandas as gpd
@@ -7,25 +7,41 @@ import os
 import pandas as pd
 import rasterio as rio
 import rasterio.mask as rmsk
+import sys
 
-catch_df = gpd.read_file(
-    "gw_catchments_reaches_filtered_addedAttributes_crosswalked.gpkg",
-    layer='gw_catchments_reaches_filtered_addedAttributes_crosswalked',
-)
+from glob import glob
 
-# Individual HUC8 Unit
-hand_healed_raster_fpath = "rem_zeroed_masked_healed.tif"
+# different filenames for 3m Fathom DEM and 10m USGS DEM
 
-src_hand_healed = rio.open(hand_healed_raster_fpath)
-in_meta = src_hand_healed.meta
+# 10m USGS
+if "10m" in os.getcwd():
+    catch_df_file = glob("*_catch.sqlite")[0]
+    hand_raster_fpath = glob("*hand.tif")[0]
+    segment_feature_name = "comid"
+
+elif "3m" in os.getcwd():
+    catch_df_file = (
+        "gw_catchments_reaches_filtered_addedAttributes_crosswalked.gpkg"
+    )
+    hand_raster_fpath = "rem_zeroed_masked_healed.tif"
+    segment_feature_name = "HydroID"
+
+else:
+    print("incorrect directory structure")
+    sys.exit()
+
+catch_df = gpd.read_file(catch_df_file)
+
+src_hand = rio.open(hand_raster_fpath)
+in_meta = src_hand.meta
 
 if not os.path.isdir("cropped_hand"):
     os.mkdir("cropped_hand")
 
-for HydroID in catch_df["HydroID"]:
-    geo = catch_df["geometry"].loc[catch_df["HydroID"] == HydroID].item()
+for seg_feat in catch_df[segment_feature_name]:
+    geo = catch_df["geometry"].loc[catch_df[segment_feature_name] == seg_feat].item()
 
-    out_image, out_transform = rmsk.mask(src_hand_healed, [geo], crop=True)
+    out_image, out_transform = rmsk.mask(src_hand, [geo], crop=True)
     out_meta = in_meta.copy()
     out_meta.update(
         {
@@ -37,10 +53,6 @@ for HydroID in catch_df["HydroID"]:
     )
 
     with rio.open(
-        f"cropped_hand/healed_FATSGTID_{HydroID}.tif", "w", **out_meta
+        f"cropped_hand/hand_{seg_feat}.tif", "w", **out_meta
     ) as dst:
         dst.write(out_image)
-
-
-
-
