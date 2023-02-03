044a64e7b47d09665d2995195f7bfa44042df7cb
markwang0 <71631906+markwang0@users.noreply.github.com>
2023-02-03 14:54:35 -0600

    include FATSGTID and feature_id

--- all_src_hydrotable_lmtQ.py
+++ all_src_hydrotable_lmtQ.py
@@ -4,7 +4,9 @@ from glob import glob
 ht_paths = glob("./3m/12*/hydroTable_rp_bf_lmtdischarge_cda.csv")
 pd.concat(
     [
-        pd.read_csv(p, usecols=["FATSGTID", "stage", "discharge_cms"])
+        pd.read_csv(
+            p, usecols=["FATSGTID", "feature_id", "stage", "discharge_cms"]
+        )
         for p in ht_paths
     ]
 ).to_csv("./hydroTable_all_ID_stage_Q.csv", index=False)
