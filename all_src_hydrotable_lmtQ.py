0550e73c2e717db2f926e7ca80f35d5abde267c8
markwang0 <71631906+markwang0@users.noreply.github.com>
2023-02-03 14:31:48 -0600

    3m res filepaths

--- all_src_hydrotable_lmtQ.py
+++ all_src_hydrotable_lmtQ.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from glob import glob
 
-ht_paths = glob("./12*/hydroTable_rp_bf_lmtdischarge_cda.csv")
-pd.concat([pd.read_csv(p) for p in ht_paths]).to_csv(
-    "./hydroTable_all_rp_bf_lmtdischarge_cda.csv",
-    index=False,
-)
+ht_paths = glob("./3m/12*/hydroTable_rp_bf_lmtdischarge_cda.csv")
+pd.concat(
+    [
+        pd.read_csv(p, usecols=["FATSGTID", "stage", "discharge_cms"])
+        for p in ht_paths
+    ]
+).to_csv("./hydroTable_all_ID_stage_Q.csv", index=False)
