43ce29efda4390190b421168217c036ab5a2a834
markwang0 <71631906+markwang0@users.noreply.github.com>
2023-02-03 12:28:55 -0600

    3m res filepaths

--- inun_launcher_prep.sh
+++ inun_launcher_prep.sh
@@ -21,7 +21,7 @@ if [ -f inun_launcher.sh ]; then
     rm inun_launcher.sh
 fi
 
-for D in 12*; do
+for D in 3m/12*; do
     if [ -d "${D}" ]; then
         mkdir -p ${D}/inundation/${name}
         echo "python calc_inun.py \"${D}\" \"${name}\"" >> inun_launcher.sh
