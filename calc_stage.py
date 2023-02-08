1c7440aeaaf19a4ca5c160323a02149cbe20d29a
markwang0 <71631906+markwang0@users.noreply.github.com>
2023-02-08 15:04:33 -0600

    calc river stage for study events

--- calc_stage.py
+++ calc_stage.py
@@ -3,44 +3,48 @@ import os
 import sys
 import pandas as pd
 
-# read in corrected streamflows
+events = ["harvey", "ike", "imelda"]
+# event = "harvey"
 
-df = pd.read_csv(
-    "./AustinExerciseModules_rev/20221221_FATSGTID_flows_all_modules.csv",
-)
+for event in events:
 
-df.index = df["FATSGTID"]
-df.drop(columns="FATSGTID", inplace=True)
+    # flowrates for a storm in SETX. feature_id and FATSGTID
+    df = pd.read_csv(
+        f"./done/{event}_max_setx_FATSGTID.csv",
+        usecols=["FATSGTID", "streamflow"],
+    )
 
-# read in master synthetic rating curves csv
-hydrotable = pd.read_csv(
-    "./AustinExerciseModules_rev/hydroTable_all_rp_bf_lmtdischarge_cda.csv",
-    usecols=["FATSGTID", "stage", "discharge_cms"],
-)
-hydrotable.index = hydrotable["FATSGTID"]
-hydrotable.drop(columns="FATSGTID", inplace=True)
+    df.index = df["FATSGTID"]
+    df.drop(columns="FATSGTID", inplace=True)
 
-# interpolate stage from streamflows with synthetic rating table
-for name in ["1", "2a", "2b", "3a", "3b"]:
+    # read in master synthetic rating curves csv
+    hydrotable = pd.read_csv(
+        "./done/hydroTable_all_ID_stage_Q.csv",
+        usecols=["FATSGTID", "feature_id", "stage", "discharge_cms"],
+    )
+    hydrotable.index = hydrotable["FATSGTID"]
+    hydrotable.drop(columns="FATSGTID", inplace=True)
 
-    mod_df = pd.DataFrame()
-    mod_df.index = df.index
-    mod_df["streamflow"] = df[f"module{name}_cms"]
-    mod_df["stage_m"] = np.nan
+    # interpolate stage from streamflows with synthetic rating table
+    event_df = pd.DataFrame()
+    event_df.index = df.index
+    event_df["Q_cms"] = df["streamflow"]
+    event_df["stage_m"] = np.nan
 
-    for hydroid in mod_df.index:
+    for hydroid in event_df.index:
+        # HydroID = FATSGTID
         # for each HydroID, subset hydrotable.csv
         # to get the HydroID specific rating table
         # and interpolate stage from discharge
         stage = np.interp(
-            mod_df.loc[hydroid, "streamflow"],
+            event_df.loc[hydroid, "Q_cms"],
             hydrotable.loc[hydroid, "discharge_cms"],
             hydrotable.loc[hydroid, "stage"],
         )
-        mod_df.loc[hydroid, "stage_m"] = stage
+        event_df.loc[hydroid, "stage_m"] = stage
 
-    mod_df.to_csv(
-        f"./AustinExerciseModules_rev/20221221_mod{name}_stage.csv",
+    event_df.to_csv(
+        f"./done/{event}_stage.csv",
     )
 
-    del mod_df
+    del event_df
