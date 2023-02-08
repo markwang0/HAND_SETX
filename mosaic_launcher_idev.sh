b3d3dd93f8cd4ce0574e41f20d28682b921523c2
markwang0 <71631906+markwang0@users.noreply.github.com>
2023-02-08 16:54:57 -0600

    update mosaic launcher scripts

--- mosaic_launcher_idev.sh
+++ mosaic_launcher_idev.sh
@@ -2,7 +2,7 @@
 
 conda activate gis-env
 module load launcher
-export LAUNCHER_WORKDIR=${pwd}
-export LAUNCHER_JOB_FILE=${pwd}/mosaic_launcher.sh
+export LAUNCHER_WORKDIR="$(pwd)"
+export LAUNCHER_JOB_FILE="$(pwd)/mosaic_launcher.sh"
 $LAUNCHER_DIR/paramrun
 
--- mosaic_launcher_prep.sh
+++ mosaic_launcher_prep.sh
@@ -1,27 +1,24 @@
 #!/usr/bin/env bash
 
-# make subdirectories for inudnation .tif files
+# make subdirectories for inundation .tif files
 # create file of launcher commands
 
 if [ -f mosaic_launcher.sh ]; then
     rm mosaic_launcher.sh
 fi
 
-# calculation identifier, change each run
-# using flows sent from Tim on 2022-12-21
-export calc_id=20221221
-mkdir mosaic_${calc_id}
-mods=("1 2a 2b 3a 3b")
-for D in 12*; do
+export mosaic_dir=/scratch/08291/mwa/doe_data/mosaic
+
+events=("harvey ike imelda")
+
+for D in 3m/12*; do
     if [ -d "${D}" ]; then
-        for i in ${mods[@]}; do
-            if [ -d "${D}"/inundation_${calc_id}/mod"${i}" ]; then
-                echo "gdalwarp -dstnodata -9999 \
+        for event in ${events[@]}; do
+            echo "gdalwarp -dstnodata -9999 \
 -srcnodata -9999 \
 -co \"COMPRESS=LZW\" \
-${D}/inundation_${calc_id}/mod${i}/healed_*.tif \
-mosaic_${calc_id}/${D}_mod${i}_healed_mosaic.tif" >> mosaic_launcher.sh
-            fi
+${D}/inundation/${event}/inun_*.tif \
+$mosaic_dir/${event}_mosaic_${D:3:-1}.tif" >> mosaic_launcher.sh
         done
     fi
 done
@@ -34,12 +31,11 @@ if [ -f full_mosaic_launcher.sh ]; then
 fi
 
 # mosaic together individual HUC8 maps
-mkdir full_mosaic_${calc_id}
-for i in ${mods[@]}; do
+for event in ${events[@]}; do
     echo "gdalwarp -dstnodata -9999 \
 -srcnodata -9999 \
 -co \"COMPRESS=LZW\" \
-mosaic_${calc_id}/*_mod${i}_healed_mosaic.tif \
-full_mosaic_${calc_id}/mod${i}_healed_full_mosaic.tif" >> full_mosaic_launcher.sh
+$mosaic_dir/${event}_mosaic_*.tif \
+$mosaic_dir/${event}_full_mosaic.tif" >> full_mosaic_launcher.sh
 done
 
