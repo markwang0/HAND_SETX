dbd69d9f520ba76896c2024427744cfc496ae8bd
markwang0 <71631906+markwang0@users.noreply.github.com>
2023-02-15 14:19:38 -0600

    aggregate HUC8 hydroID-areasqkm table

---
+++ all_hydroid_areasqkm.py
@@ -0,0 +1,35 @@
+import geopandas as gpd
+import pandas as pd
+
+from glob import glob
+
+catch_paths = glob(
+    "./3m/12*/gw_catchments_reaches_filtered_addedAttributes_crosswalked.gpkg"
+)
+
+# concatenate together HydroID and areasqkm lists for each HUC8
+pd.concat(
+    [
+        gpd.read_file(
+            p,
+            ignore_geometry=True,
+            ignore_fields=[
+                # "HydroID",
+                "S0",
+                "LakeID",
+                "LengthKm",
+                "From_Node",
+                "To_Node",
+                "NextDownID",
+                "min_thal_elev",
+                "med_thal_elev",
+                "max_thal_elev",
+                # "areasqkm",
+                "feature_id",
+                "order_",
+                # "geometry",
+            ],
+        )
+        for p in catch_paths
+    ]
+).to_csv("./all_hydroid_areasqkm.csv", index=False)
